50f34853cb5eaa24551eaf3fcb6ad21efc9a716e
Aaron <aaronyinyong@hotmail.com>
2019-03-08 17:14:03 +0800

    upload test2.py

--- Test/TestJointQuant1.py
+++ Test/TestJointQuant1.py
@@ -1,4 +1,16 @@
 
 #https://www.joinquant.com/help/api/help?name=JQData#%E5%A6%82%E4%BD%95%E5%AE%89%E8%A3%85%E4%BD%BF%E7%94%A8JQData
 
+import jqdatasdk
+
 from jqdatasdk import *
+
+jqdatasdk.auth('13651829783', 'aaronjoinquant')
+
+#print(__version__)
+
+#print(get_query_count())
+
+q=query(finance.STK_FIN_FORCAST).filter(finance.STK_FIN_FORCAST.code=='300383.XSHE',finance.STK_FIN_FORCAST.pub_date>='2018-07-01').limit(10)
+df=finance.run_query(q)
+print(df)
